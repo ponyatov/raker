b59d72d5219c49243ef1911886ef38928e6cd0df
Dmitry Ponyatov <dponyatov@gmail.com>
2020-01-31 23:56:29 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ raker.m
@@ -0,0 +1,12 @@
+module  raker
+title   legacy code raker
+author  Dmitry Ponyatov <dponyatov@gmail.com>
+license MIT 2020
+github  https://github.com/ponyatov/raker
+
+file    raker.m
+file    README.md
+file    Makefile
+file    .gitignore
+
+file    raker.ml
